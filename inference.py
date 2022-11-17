5c9dcc76485e17ec76cfb0ceb849c48bc26189da
sahil095 <sehgal.sahil0786@gmail.com>
2022-11-17 10:14:41 -0500

    q1 done

--- inference.py
+++ inference.py
@@ -186,7 +186,19 @@ class InferenceModule:
         Return the probability P(noisyDistance | pacmanPosition, ghostPosition).
         """
         "*** YOUR CODE HERE ***"
-        raiseNotDefined()
+        if ghostPosition == jailPosition:
+            if noisyDistance == None:
+                return 1.0
+            else:
+                return 0.0
+        
+        if noisyDistance == None:
+            return 0.0
+
+        trueDistance = manhattanDistance(pacmanPosition, ghostPosition)
+        prob = busters.getObservationProbability(noisyDistance, trueDistance)
+        return prob        
+        # raiseNotDefined()
 
     def setGhostPosition(self, gameState, ghostPosition, index):
         """
